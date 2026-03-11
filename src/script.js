815f4f7d0153af5772b2c39598bee0868e387e91
duff-e <elise.duffy@live.com>
2026-03-11 20:10:14 +1000

    improved user experience

--- src/script.js
+++ src/script.js
@@ -11,11 +11,12 @@ function createItinerary(event) {
   let userInput = document.querySelector("#city-input");
   let apiKey = "40o03736bbe0e6faa9b79f5dt4af0a0f";
   let context =
-    "You are a global travel expert and experienced travel guide. List 3 must go to attractions to see when visiting a city. Display using basic HTML <ul> <li> ";
+    "You are a global travel expert and experienced travel guide. List 3 must go to attractions to see when visiting a city. Display using basic HTML <ul> <li> and the attractions <strong> ";
   let prompt = `Name the must see attractions for ${userInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let loadingItinerary = document.querySelector("#itinerary");
+  loadingItinerary.classList.remove("hidden");
   loadingItinerary.innerHTML = `🧳✈️ Generating a travel itinerary for ${userInput.value}`;
 
   axios.get(apiUrl).then(displayItinerary);
